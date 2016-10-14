219989501246aa3b5cd4725f2c019d41616d14d8
shahuwang <shahuwang@qq.com>
2016-10-15 01:07:23 +0800

    update

---
+++ gocalc.y
@@ -0,0 +1,77 @@
+%{
+package main
+import(
+    "fmt"
+    "bufio"
+    "os"
+)
+const(
+GoCalcDEBUG=1
+)
+%}
+
+%union {
+    int_value int
+    float_value float64
+}
+
+%token <float_value> DOUBLE_LITERAL
+%token ADD SUB MUL DIV CR LP RP
+
+%type <float_value> expression term primary_expression
+
+%%
+line_list
+    : line
+    | line_list line
+    ;
+line
+    : expression CR
+    {
+        fmt.Printf(">>%1f\n", $1);
+    }
+    ;
+expression
+    : term
+    | expression ADD term
+    {
+        $$ = $1 + $3;
+    }
+    | expression SUB term
+    {
+        $$ = $1 - $3;
+    }
+    ;
+term
+    : primary_expression
+    | term MUL primary_expression
+    {
+        $$ = $1 * $3;
+    }
+    | term DIV primary_expression
+    {
+        $$ = $1 / $3;
+    }
+    ;
+primary_expression
+    : DOUBLE_LITERAL
+    | LP expression RP
+    {
+        $$ = $2;
+    }
+    | SUB primary_expression
+    {
+        $$ = -$2;
+    }
+    ;
+%%
+
+func main(){
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan(){
+        text := scanner.Text()
+        text = fmt.Sprintf("%s\n", text)
+        GoCalcParse(&GoCalcLex{Input: []byte(text)})
+    }
+}
+
---
+++ golex.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"unicode"
+	"unicode/utf8"
+)
+
+type LexerStatus int
+
+const (
+	INITIAL_STATUS LexerStatus = iota
+	IN_INT_PART_STATUS
+	DOT_STATUS
+	IN_FRAC_PART_STATUS
+)
+
+type GoCalcLex struct {
+	Pos    int
+	Status LexerStatus
+	Input  []byte
+}
+
+func (l *GoCalcLex) Lex(lval *GoCalcSymType) int {
+	var Str []rune
+	l.Status = INITIAL_STATUS
+	for l.Pos < len(l.Input) {
+		r, n := utf8.DecodeRune(l.Input[l.Pos:])
+		if (l.Status == IN_INT_PART_STATUS || l.Status == IN_FRAC_PART_STATUS) &&
+			!unicode.IsDigit(r) && r != '.' {
+			lval.float_value, _ = strconv.ParseFloat(string(Str), 64)
+			return DOUBLE_LITERAL
+		}
+		if unicode.IsSpace(r) {
+			l.Pos = l.Pos + n
+			if r == '\n' || r == '\r' {
+				return CR
+			}
+			continue
+		}
+		Str = append(Str, r)
+		l.Pos = l.Pos + n
+		switch {
+		case r == '+':
+			return ADD
+		case r == '-':
+			return SUB
+		case r == '*':
+			return MUL
+		case r == '/':
+			return DIV
+		case r == '(':
+			return LP
+		case r == ')':
+			return RP
+		case r == '.':
+			if l.Status == IN_INT_PART_STATUS {
+				l.Status = DOT_STATUS
+			} else {
+				fmt.Println("syntax error in dot")
+				os.Exit(1)
+			}
+		case unicode.IsDigit(r):
+			if l.Status == INITIAL_STATUS {
+				l.Status = IN_INT_PART_STATUS
+			} else if l.Status == DOT_STATUS {
+				l.Status = IN_FRAC_PART_STATUS
+			}
+		default:
+			fmt.Println("unknow input type %s", string(r))
+			os.Exit(1)
+		}
+	}
+	return 0
+}
+
+func (l *GoCalcLex) Error(s string) {
+	fmt.Printf("syntax error: %s\n", s)
+}
